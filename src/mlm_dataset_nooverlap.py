c2b5d8c3c127f6e13e35b2df260531f4705744ab
Tom556 <t.limisiewicz@gmail.com>
2022-12-09 16:23:27 +0100

    Training model with nooverlap between languages, special tokens shared

---
+++ src/mlm_dataset_nooverlap.py
@@ -0,0 +1,203 @@
+from tqdm import tqdm
+import numpy as np
+from transformers import set_seed
+import os
+from torch.utils.data import Dataset
+from dataclasses import dataclass
+from typing import Any, Callable, Dict, List, NewType, Optional, Tuple, Union
+import torch
+from transformers.tokenization_utils_base import BatchEncoding, PreTrainedTokenizerBase
+import logging
+rng = np.random.RandomState(2021)
+
+
+class LineByLineTextDataset(Dataset):
+    def __init__(self, lang_to_tokenizer, file_paths, block_size, truncate_at=-1, name="", randomize=True, rand_seed=10, is_eval=False, language_codes=None, lang_to_offset=None):
+        rng.seed(rand_seed)
+        logging.info(f"seed: {rand_seed}")
+        lang_ids = []
+        input_ids = []
+
+        portion = truncate_at//len(file_paths) if truncate_at!=-1 else -1
+        if language_codes:
+            logging.info(f'ids: {language_codes}')
+        for idx, file_path in enumerate(file_paths):
+            new_lines=[]
+            logging.info(file_path)
+            assert os.path.isfile(file_path), "Input file path {} not found".format(file_path)
+            with open(file_path, encoding="utf-8") as f:
+                new_lang_lines = [line for line in tqdm(f.readlines(), desc=f"reading lines {name}, is random: {randomize}") if (len(line) > 0 and not line.isspace())]
+                new_lines+=new_lang_lines
+            if portion>=0 and is_eval:
+                new_lines = new_lines[:min(portion,len(new_lines))]
+
+            new_input_ids = lang_to_tokenizer[language_codes[idx]](new_lines, add_special_tokens=True, truncation=True, max_length=block_size-2)['input_ids']
+            new_input_ids = [[tok_id + lang_to_offset.get(language_codes[idx], 0) if tok_id > 4 else tok_id for tok_id in ii] for ii in new_input_ids]
+            input_ids += new_input_ids
+
+            if language_codes is not None:
+                lang_ids += [language_codes[idx]]*len(new_lines)
+
+        if language_codes is not None:
+            assert len(input_ids) == len(lang_ids)
+
+        lang_ids = np.array(lang_ids)
+        input_ids = np.array(input_ids)
+
+        indices = np.arange(len(input_ids))
+        if randomize:
+            rng.shuffle(indices)
+
+        if truncate_at >= 1:
+            indices=indices[:truncate_at]
+
+        input_ids = input_ids[indices].tolist()
+
+        if language_codes is not None:
+            lang_ids = lang_ids[indices].tolist()
+        self.examples = input_ids
+        if language_codes is not None:
+            self.examples = [{"input_ids": torch.tensor(self.examples[i], dtype=torch.long), "language_ids":lang_ids[i]} for i in tqdm(range(len(indices)), desc=f"extracting tokenized lines {name}, order:{indices[:10]}... with ids")]
+        else:
+            self.examples = [{"input_ids": torch.tensor(self.examples[i], dtype=torch.long), "language_ids":-1} for i in tqdm(range(len(indices)), desc=f"extracting tokenized lines {name}, order:{indices[:10]}...")]
+
+    def __len__(self):
+        return len(self.examples)
+
+    def __getitem__(self, i):
+        return self.examples[i]
+
+
+def _collate_batch(examples, pad_token_id):
+    """Collate `examples` into a batch, using the information in `tokenizer` for padding if necessary."""
+    # Tensorize if necessary.
+    if isinstance(examples[0], (list, tuple)):
+        examples = [torch.tensor(e, dtype=torch.long) for e in examples]
+
+    # Check if padding is necessary.
+    length_of_first = examples[0].size(0)
+    are_tensors_same_length = all(x.size(0) == length_of_first for x in examples)
+    if are_tensors_same_length:
+        return torch.stack(examples, dim=0)
+
+    # If yes, check if we have a `pad_token`.
+    # if tokenizer._pad_token is None:
+    #     raise ValueError(
+    #         "You are attempting to pad samples but the tokenizer you are using"
+    #         f" ({tokenizer.__class__.__name__}) does not have a pad token."
+    #     )
+
+    # Creating the full tensor and filling it with our data.
+    max_length = max(x.size(0) for x in examples)
+    result = examples[0].new_full([len(examples), max_length], pad_token_id)
+    for i, example in enumerate(examples):
+        logging.info(f"padding side is always from the right.")
+        result[i, : example.shape[0]] = example
+    return result
+
+
+def tolist(x: Union[List[Any], torch.Tensor]):
+    return x.tolist() if isinstance(x, torch.Tensor) else x
+
+@dataclass
+class DataCollatorForLanguageModeling:
+    """
+    Data collator used for language modeling. Inputs are dynamically padded to the maximum length of a batch if they
+    are not all of the same length.
+
+    Args:
+        tokenizer (:class:`~transformers.PreTrainedTokenizer` or :class:`~transformers.PreTrainedTokenizerFast`):
+            The dictionary mapping language code to tokenizer.
+        mlm (:obj:`bool`, `optional`, defaults to :obj:`True`):
+            Whether or not to use masked language modeling. If set to :obj:`False`, the labels are the same as the
+            inputs with the padding tokens ignored (by setting them to -100). Otherwise, the labels are -100 for
+            non-masked tokens and the value to predict for the masked token.
+        mlm_probability (:obj:`float`, `optional`, defaults to 0.15):
+            The probability with which to (randomly) mask tokens in the input, when :obj:`mlm` is set to :obj:`True`.
+
+    .. note::
+
+        For best performance, this data collator should be used with a dataset having items that are dictionaries or
+        BatchEncoding, with the :obj:`"special_tokens_mask"` key, as returned by a
+        :class:`~transformers.PreTrainedTokenizer` or a :class:`~transformers.PreTrainedTokenizerFast` with the
+        argument :obj:`return_special_tokens_mask=True`.
+    """
+
+    #lang_to_tokenizer: dict[PreTrainedTokenizerBase]
+    # TODO: think how to make special tokens language dependent
+    tokenizer: PreTrainedTokenizerBase
+    # tokenizer.mask_token_id: int
+    # tokenizer.pad_token_id: int
+    # tokenizer.cls_token_id: int
+    # tokenizer.sep_token_id: int
+    vocab_size: int
+    mlm: bool = True
+    mlm_probability: float = 0.15
+
+    def __post_init__(self):
+        if self.mlm and self.tokenizer.mask_token_id is None:
+            raise ValueError(
+                "This tokenizer does not have a mask token which is necessary for masked language modeling. "
+                "You should pass `mlm=False` to train on causal language modeling instead."
+            )
+
+    def __call__(
+            self, examples: List[Union[List[int], torch.Tensor, Dict[str, torch.Tensor]]]
+    ) -> Dict[str, torch.Tensor]:
+        # Handle dict or lists with proper padding and conversion to tensor.
+        # TODO: think how to handle padding when the tokenizers differ for different languages ?
+        
+        if isinstance(examples[0], (dict, BatchEncoding)):
+            batch = self.tokenizer.pad(examples, return_tensors="pt")
+            # raise NotImplementedError("BatchEncoding suppoert not implemented yet")
+        else:
+            batch = {"input_ids": _collate_batch(examples, self.tokenizer.pad_token_id)}
+
+        # If special token mask has been preprocessed, pop it from the dict.
+        special_tokens_mask = batch.pop("special_tokens_mask", None)
+        if self.mlm:
+            batch["input_ids"], batch["labels"] = self.mask_tokens(
+                batch["input_ids"], special_tokens_mask=special_tokens_mask
+            )
+        else:
+            labels = batch["input_ids"].clone()
+            if self.tokenizer.pad_token_id is not None:
+                labels[labels == self.tokenizer.pad_token_id] = -100
+            batch["labels"] = labels
+        return batch
+
+    def mask_tokens(
+            self, inputs: torch.Tensor, special_tokens_mask: Optional[torch.Tensor] = None
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        """
+        Prepare masked tokens inputs/labels for masked language modeling: 80% MASK, 10% random, 10% original.
+        """
+        labels = inputs.clone()
+        # We sample a few tokens in each sequence for MLM training (with probability `self.mlm_probability`)
+        probability_matrix = torch.full(labels.shape, self.mlm_probability)
+        if special_tokens_mask is None:
+            special_tokens_mask = [
+                list(map(lambda x: 1 if x in [self.tokenizer.sep_token_id, self.tokenizer.cls_token_id, self.tokenizer.pad_token_id] else 0, val)) for val in labels.tolist()
+            ]
+            special_tokens_mask = torch.tensor(special_tokens_mask, dtype=torch.bool)
+        else:
+            special_tokens_mask = special_tokens_mask.bool()
+
+        probability_matrix.masked_fill_(special_tokens_mask, value=0.0)
+        masked_indices = torch.bernoulli(probability_matrix).bool()
+        labels[~masked_indices] = -100  # We only compute loss on masked tokens
+
+        # 80% of the time, we replace masked input tokens with tokenizer.mask_token ([MASK])
+        indices_replaced = torch.bernoulli(torch.full(labels.shape, 0.8)).bool() & masked_indices
+        inputs[indices_replaced] = self.tokenizer.mask_token_id
+
+        # 10% of the time, we replace masked input tokens with random word
+        indices_random = torch.bernoulli(torch.full(labels.shape, 0.5)).bool() & masked_indices & ~indices_replaced
+        random_words = 5 + torch.randint(self.vocab_size - 5, labels.shape, dtype=torch.long)
+        inputs[indices_random] = random_words[indices_random]
+
+        # The rest of the time (10% of the time) we keep the masked input tokens unchanged
+        return inputs, labels
+    
+    # TODO: Mapping input tokens function based on language indices
+
---
+++ src/train_mlm_nooverlap.py
@@ -0,0 +1,178 @@
+import torch
+import argparse
+from pynvml import nvmlInit, nvmlDeviceGetHandleByIndex, nvmlDeviceGetMemoryInfo
+
+from transformers import set_seed
+from transformers import XLMRobertaTokenizerFast
+from transformers import XLMRobertaConfig, XLMRobertaForMaskedLM
+
+
+import logging
+import sys
+import os, pickle
+import json
+from mlm_dataset_nooverlap import LineByLineTextDataset, DataCollatorForLanguageModeling
+from transformers import TrainingArguments, EarlyStoppingCallback, IntervalStrategy, Trainer
+from eval import compute_metrics
+
+logging.basicConfig(level=logging.INFO)
+logging.info(torch.cuda.is_available())
+
+
+def pretrain(pretrain_outpath, model_config, pt_config, truncate_at, load_checkpoint=True, data_seed=10, seed=10, eval_and_save_steps=5000, early_stopping_patience=2, initial_learning_rate=5e-5, gradient_accumulation_steps=8, fp16=True, gradient_checkpointing=False):
+    set_seed(seed)
+
+    logging.info("Loading tokenizer..")
+    # get tokenizer:
+    tokenizers = [XLMRobertaTokenizerFast.from_pretrained(tp, max_len=model_config['max_sent_len']) for tp in model_config['tokenizer_path']]
+    # TODO: allow for different size tokenizer for each language (low priority)
+    assert all([len(tokenizer) == len(tokenizers[0]) for tokenizer in tokenizers]), "All tokenizers must have the same vocabulary size"
+    
+    languages = model_config['tokenizer_lang']
+    lang_to_tokenizers = {lang:tokenizer for lang, tokenizer in zip(languages, tokenizers)}
+    lang_to_offset= {lang: len(tokenizers[0]) * i for i, lang in enumerate(languages)}
+    
+    MASK_ID = tokenizers[0].mask_token_id # [si + t.mask_token_id for t, si in zip(tokenizers, language_start_indices)]
+    logging.info(MASK_ID)
+    # build dataset:
+    logging.info("Building Model..")
+    
+    config = XLMRobertaConfig(
+        vocab_size=model_config['vocab_size'] *  len(tokenizers),
+        hidden_size=model_config['hidden_layer_size'],
+        num_hidden_layers=model_config['num_hidden'],
+        num_attention_heads=model_config['num_attention'],
+        max_position_embeddings=model_config['max_sent_len']
+    )
+    model = XLMRobertaForMaskedLM(config)
+    # define a data_collator (a small helper that will help us batch different samples of the dataset together into an
+    # object that PyTorch knows how to perform backprop on):
+    data_collator = DataCollatorForLanguageModeling(
+        tokenizer=tokenizers[0],
+        # mask_token_id=tokenizers[0].mask_token_id,
+        # pad_token_id=tokenizers[0].pad_token_id,
+        # cls_token_id=tokenizers[0].cls_token_id,
+        # sep_token_id=tokenizers[0].sep_token_id,
+        vocab_size=len(tokenizers[0]) * len(tokenizers),
+        mlm=True, mlm_probability=0.15
+    )
+    # init trainer:
+    logging.info("Training\Loading model...")
+    logging.info(f"#params:, {model.num_parameters()}")
+    if torch.cuda.is_available():
+        logging.info(f"#memory used:, {memory_used_in_mb()} MB")
+
+    if not os.path.exists(os.path.join(pretrain_outpath,'config.json')):
+        logging.info("Loading pretrain data..")
+
+        truncate_eval = 1000 if truncate_at == -1 else min(truncate_at,1000)
+        pretrain_dataset = LineByLineTextDataset(lang_to_tokenizer=lang_to_tokenizers, file_paths= pt_config['train_data_paths_list'], block_size=model_config['max_sent_len'], truncate_at=truncate_at, name="pretrain train", rand_seed=data_seed, language_codes=languages,lang_to_offset=lang_to_offset, is_eval=False)
+        preeval_dataset = LineByLineTextDataset(lang_to_tokenizer=lang_to_tokenizers, file_paths=pt_config['eval_data_paths_list'], block_size=model_config['max_sent_len'], truncate_at=truncate_eval, name="pretrain eval", rand_seed=data_seed, language_codes=languages,lang_to_offset=lang_to_offset, is_eval=True)
+
+        logging.info("Pretraining model..")
+        os.makedirs(pretrain_outpath, exist_ok=True)
+        training_args = TrainingArguments(
+            output_dir=pretrain_outpath,
+            overwrite_output_dir=True,
+            num_train_epochs=pt_config['num_epochs'],
+            per_device_train_batch_size=pt_config['batch_size']//gradient_accumulation_steps,
+            gradient_accumulation_steps=gradient_accumulation_steps,
+            fp16=(fp16 and torch.cuda.is_available()),
+            gradient_checkpointing=gradient_checkpointing,
+            save_steps=eval_and_save_steps,
+            per_device_eval_batch_size=64,
+            save_total_limit=5,
+            report_to=['tensorboard'],
+            logging_steps=max(eval_and_save_steps//100, 10),
+            eval_steps=eval_and_save_steps,
+            evaluation_strategy=IntervalStrategy.STEPS,
+            eval_accumulation_steps=1,
+            metric_for_best_model='mrr',
+            greater_is_better=True,
+            load_best_model_at_end=True,
+            learning_rate=initial_learning_rate,
+            warmup_ratio=0.01
+        )
+        logging.info(f"Reporting to: {training_args.report_to}")
+        trainer = Trainer(
+            model=model,
+            args=training_args,
+            data_collator=data_collator,
+            train_dataset=pretrain_dataset,
+            eval_dataset=preeval_dataset,
+            compute_metrics=compute_metrics,
+            callbacks=[EarlyStoppingCallback(early_stopping_patience=early_stopping_patience, early_stopping_threshold=0.0)]
+        )
+        if load_checkpoint:
+            logging.info("loading pt checkpoint")
+            try:
+                trainer.train(resume_from_checkpoint=True)
+            except Exception as e:
+                logging.info("Failed loading checkpoint, regular training")
+                trainer.train()
+        else:
+            logging.info("training pt from scratch")
+            trainer.train()
+        trainer.save_model(pretrain_outpath)
+        logging.info(f"Done pretrain. pretrained model saved in: {pretrain_outpath} \n")
+        metrics = trainer.evaluate()
+        logging.info(metrics)
+
+        with open(os.path.join(pretrain_outpath,'pretrain_eval.pickle'), 'wb') as evalout:
+            pickle.dump(metrics, evalout, protocol=pickle.HIGHEST_PROTOCOL)
+
+        with open(sys.argv[0], 'r') as model_code, open(os.path.join(pretrain_outpath,'pretrain_source_code.py'), 'w') as source_out :
+            code_lines = model_code.readlines()
+            source_out.writelines(code_lines)
+
+        logging.info("Only pretrain, Done.")
+    else:
+        logging.info(f"model exists: {pretrain_outpath}")
+
+
+def load_config(config_path):
+    with open(config_path, 'r') as fp:
+        return json.load(fp)
+
+
+def memory_used_in_mb():
+    nvmlInit()
+    handle = nvmlDeviceGetHandleByIndex(0)
+    info = nvmlDeviceGetMemoryInfo(handle)
+    return info.used//1024**2
+
+
+def train(args):
+    out_path = args.out
+    pretrain_name = args.pretrain_name
+    data_seed = args.data_seed
+    seed = args.seed
+    logging.info('Training mulitling')
+    mask_loss = args.mask_loss
+    pretrain_outpath = os.path.join(out_path, pretrain_name+'_'+str(data_seed))
+    logging.info(f"Common pretrain path: {pretrain_outpath}")
+
+    model_config = load_config(args.model_config_path)
+    pt_config = load_config(args.pretrain_config_path)
+    
+    pretrain(pretrain_outpath, model_config, pt_config, args.truncate_at, args.load_checkpoint, data_seed, seed, eval_and_save_steps=args.eval_and_save_steps, early_stopping_patience=args.early_stopping_patience, initial_learning_rate=args.initial_learning_rate)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-o', '--out', type=str, required=True)
+    parser.add_argument('-p', '--pretrain_name', type=str, required=True)
+    parser.add_argument('--model_config_path',type=str, required=True)
+    parser.add_argument('--pretrain_config_path',type=str, required=True)
+    parser.add_argument('--truncate_at',type=int, required=False, default=-1)
+    parser.add_argument('--load_checkpoint',type=bool, required=False, default=True)
+    parser.add_argument('--data_seed',type=int, required=False, default=10)
+    parser.add_argument('--seed',type=int, required=False, default=10)
+    parser.add_argument('--eval_and_save_steps', type=int, required=False, default=5000)
+    parser.add_argument('--early_stopping_patience', type=int, required=False, default=20)
+    parser.add_argument('--initial_learning_rate', type=float, required=False, default=5e-5)
+    parser.add_argument('--mask_loss',type=bool, required=False, default=False)
+    args = parser.parse_args()
+    logging.info(vars(args))
+    train(args)
+
